9203e50a1f0f67f2bc183e052553b8d18e9d521a
Sean Greene <greenese@dc22.cis.gvsu.edu>
2018-10-08 04:51:57 -0400

    Fixed compilation error

--- Boss.cpp
+++ Boss.cpp
@@ -9,6 +9,7 @@ Boss::Boss() {
 		exit(EXIT_FAILURE);
 	}
 	texture.loadFromImage(spritesheet, sf::IntRect(0,0,31,34));
+	sf::FloatRect spriteSize = sprite.getGlobalBounds();
 	sprite.setOrigin(spriteSize.width/4.0,spriteSize.height/4.0);
 	sprite.setTexture(texture);
 	sprite.setColor(sf::Color(255, 130, 0, 255));
