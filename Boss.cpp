2f0b22ee9fffbaf0c5bd3a23052509cdf0be3003
Sean Greene <greenese@dc22.cis.gvsu.edu>
2018-10-08 04:57:55 -0400

    Touched up Boss

--- Boss.cpp
+++ Boss.cpp
@@ -9,8 +9,6 @@ Boss::Boss() {
 		exit(EXIT_FAILURE);
 	}
 	texture.loadFromImage(spritesheet, sf::IntRect(0,0,31,34));
-	sf::FloatRect spriteSize = sprite.getGlobalBounds();
-	sprite.setOrigin(spriteSize.width/4.0,spriteSize.height/4.0);
 	sprite.setTexture(texture);
 	sprite.setColor(sf::Color(255, 130, 0, 255));
 	this->health = 50;
